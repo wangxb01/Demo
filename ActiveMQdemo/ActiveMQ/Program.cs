9ef4f609f7f72f63a7e9887b772d04dbad7a7639
wangxb <157964779@qq.com>
2020-07-24 17:30:59 +0800

    练手 AvtiveMq 消息队列

---
+++ ActiveMQdemo/ActiveMQ/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using System.Threading;
+
+namespace ActiveMQ
+{
+    /// <summary>
+    /// 生产者
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            InitProducer();
+
+            SendMsg();
+            Console.ReadLine();
+        }
+        private static IConnectionFactory factory;
+
+        public static void init()
+        {
+
+        }
+
+        public static void InitProducer()
+        {
+            try
+            {
+                //初始化工厂，这里默认的URL是不需要修改的
+                factory = new ConnectionFactory("tcp://localhost:61616");
+
+            }
+            catch
+            {
+                string Text = "初始化失败!!";
+                Console.WriteLine(Text);
+            }
+        }
+        public static void SendMsg()
+        {
+            while (true)
+            {
+                string msg = Console.ReadLine();
+                using (IConnection connection = factory.CreateConnection())
+                {
+                    //通过连接创建Session会话
+                    using (ISession session = connection.CreateSession())
+                    {
+                        //通过会话创建生产者，方法里面new出来的是MQ中的Queue
+                        IMessageProducer prod = session.CreateProducer(new Apache.NMS.ActiveMQ.Commands.ActiveMQQueue("firstQueue"));
+                        //创建一个发送的消息对象
+                        ITextMessage message = prod.CreateTextMessage();
+                        //给这个对象赋实际的消息
+                        message.Text = "消息:" + msg;
+                        //设置消息对象的属性，这个很重要哦，是Queue的过滤条件，也是P2P消息的唯一指定属性
+                        message.Properties.SetString("filter", "demo");
+                        //生产者把消息发送出去，几个枚举参数MsgDeliveryMode是否长链，MsgPriority消息优先级别，发送最小单位，当然还有其他重载
+                        prod.Send(message, MsgDeliveryMode.NonPersistent, MsgPriority.Normal, TimeSpan.MinValue);
+
+
+
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ActiveMQdemo/ActiveMQConsume/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.ActiveMQ.Commands;
+using Apache.NMS.ActiveMQ.Threads;
+
+namespace ActiveMQConsume
+{
+    /// <summary>
+    /// 消费者
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+          
+                string clientid = GenerateRandom(8);
+                InitConsumer();
+     
+            
+            Console.ReadLine();
+        }
+        public static void InitConsumer()
+        {
+          
+                //创建连接工厂
+                IConnectionFactory factory = new ConnectionFactory("tcp://localhost:61616");
+                //通过工厂构建连接
+                IConnection connection = factory.CreateConnection();
+                //这个是连接的客户端名称标识
+                connection.ClientId = "001";
+                //启动连接，监听的话要主动启动连接
+                connection.Start();
+                //通过连接创建一个会话
+                ISession session = connection.CreateSession();
+                //通过会话创建一个消费者，这里就是Queue这种会话类型的监听参数设置
+                IMessageConsumer consumer = session.CreateConsumer(new Apache.NMS.ActiveMQ.Commands.ActiveMQQueue("firstQueue?consumer.prefetchSize=1"), "filter='demo'");
+                //注册监听事件
+                consumer.Listener += (IMessage mesage) =>
+                {
+                    ITextMessage msg = (ITextMessage)mesage;
+                    Console.WriteLine(001+"接收消息:" + msg.Text);
+                };
+           
+               
+         
+           
+        }
+
+        private static char[] constant = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
+
+        public static string GenerateRandom(int Length)
+        {
+            StringBuilder newRandom = new StringBuilder();
+            Random rd = new Random();
+            for (int i = 0; i < Length; i++)
+            {
+                newRandom.Append(constant[rd.Next(8)]);
+            }
+            return newRandom.ToString();
+
+        }
+
+    }
+}
